7e77e685e6cc2b1b87b325a2aa215e8e02f0172b
coder114514 <98397499+coder114514@users.noreply.github.com>
2026-01-26 03:34:15 +0800

    Implement initial skeletal version

---
+++ src/main/java/TobTahc.java
@@ -0,0 +1,10 @@
+public class TobTahc {
+    public static void main(String[] args) {
+        System.out.println("___________________________________________________________________");
+        System.out.println("Hello! I'm TobTahc. Tob tahc a ma I.");
+        System.out.println("What can I do for you?");
+        System.out.println("___________________________________________________________________");
+        System.out.println("Noos niaga uoy ees ot epoh! Eyb.");
+        System.out.println("___________________________________________________________________");
+    }
+}
