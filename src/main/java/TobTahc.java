af38eb5be45e1b94272d607b15cbfd344c3f0a52
coder114514 <98397499+coder114514@users.noreply.github.com>
2026-01-26 10:25:12 +0800

    Implement add and list

--- src/main/java/TobTahc.java
+++ src/main/java/TobTahc.java
@@ -1,18 +1,36 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class TobTahc {
     public static void main(String[] args) {
         chatIntro();
 
+        var list = new ArrayList<String>();
         var scanner = new Scanner(System.in);
         for (;;) {
             var input = scanner.nextLine();
             if (input.equals("bye")) {
                 break;
             }
-            botMessageLine("___________________________________________________________________");
-            botMessageLine(input);
-            botMessageLine("___________________________________________________________________\n");
+            if (input.equals("list")) {
+                botMessageSep();
+                if (list.size() == 0) {
+                    botMessageLine("(empty)");
+                }
+                for (int i = 0; i < list.size(); ++i) {
+                    botMessageLine(String.format("%s: %s", i + 1, list.get(i)));
+                }
+                botMessageSep();
+                continue;
+            }
+            list.add(input);
+            botMessageSep();
+            if (list.hashCode() % 4 == 0) {
+                botMessageLine("dedda: " + input);
+            } else {
+                botMessageLine("added: " + input);
+            }
+            botMessageSep();
         }
 
         chatBye();
@@ -27,17 +45,21 @@ public class TobTahc {
         System.out.println("    " + str);
     }
 
+    public static void botMessageSep() {
+        botMessageLine("___________________________________________________________________\n");
+    }
+
     public static void chatIntro() {
-        botMessageLine("___________________________________________________________________");
+        botMessageSep();
         botMessageLine("Hello! I'm TobTahc. Tob tahc a ma I.");
         botMessageLine("What can I do for you?");
-        botMessageLine("___________________________________________________________________");
+        botMessageSep();
         botMessageLine();
     }
 
     public static void chatBye() {
-        botMessageLine("___________________________________________________________________");
+        botMessageSep();
         botMessageLine("Noos niaga uoy ees ot epoh! Eyb.");
-        botMessageLine("___________________________________________________________________");
+        botMessageSep();
     }
 }
