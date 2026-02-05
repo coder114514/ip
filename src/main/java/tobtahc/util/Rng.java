5c7cb3d6d463967a408e3df5d5963a34e49c90ef
coder114514 <98397499+coder114514@users.noreply.github.com>
2026-02-06 03:30:58 +0800

    Refine some more

--- src/main/java/tobtahc/util/Rng.java
+++ src/main/java/tobtahc/util/Rng.java
@@ -17,6 +17,9 @@ public class Rng implements RandomProvider {
 
     @Override
     public boolean chance(int nominator, int denominator) {
+        if (nominator < 0 || denominator <= 0 || nominator > denominator) {
+            throw new IllegalArgumentException("illegal arguments passed to chance()");
+        }
         return r.nextInt(denominator) < nominator;
     }
 }
