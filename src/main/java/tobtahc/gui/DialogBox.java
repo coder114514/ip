a2c0f5004f9cd14bd12551c56873abbf421316ef
coder114514 <98397499+coder114514@users.noreply.github.com>
2026-02-17 22:24:15 +0800

    Add CSS

--- src/main/java/tobtahc/gui/DialogBox.java
+++ src/main/java/tobtahc/gui/DialogBox.java
@@ -65,6 +65,7 @@ class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
+        dialog.getStyleClass().add("reply-label");
         return this;
     }
 }
